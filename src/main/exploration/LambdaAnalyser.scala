396c1868e0d609147a91dc2b03f54c5c1c30537d
irgend42 <und0m3@gmail.com>
2017-06-23 13:12:59 +0200

    added some stuff

--- src/main/exploration/LambdaAnalyser.scala
+++ src/main/exploration/LambdaAnalyser.scala
@@ -5,10 +5,11 @@ import java.nio.file.Paths
 
 import com.typesafe.scalalogging.Logger
 import exploration.KernelGenerator.parser
-import exploration.ParameterRewrite.readFromFile
+import exploration.ParameterRewrite.{readLambdaFromFile, settings, _}
+import exploration.ParameterSearch.SubstitutionMap
 import ir.TypeChecker
 import ir.ast.{Expr, Lambda}
-import lift.arithmetic.{?, ArithExpr, Range, RangeAdd, RangeUnknown, Var}
+import lift.arithmetic._
 import opencl.executor._
 import opencl.generator.NDRange
 import org.clapper.argot._
@@ -56,6 +57,50 @@ object LambdaAnalyser {
     parser.parse(args)
 
     var lambdaPath = input.value.get.toPath.toAbsolutePath.toString
+
+    //new stuff!
+    var hlLambdaPath = input.value.get.toPath.getParent.getParent.getParent //Lower dir
+    var hlName = hlLambdaPath.getFileName.toString
+    hlLambdaPath=hlLambdaPath.getParent
+    hlLambdaPath=hlLambdaPath.getParent.resolve(hlLambdaPath.getFileName.toString.dropRight("Lower".length)).resolve(hlName.substring(0,1)).resolve(hlName.substring(1,2)).resolve(hlName)
+    val settings = ParseSettings(None)
+
+
+    val high_level_expr_orig = readLambdaFromFile(hlLambdaPath.toString)
+
+    val vars = high_level_expr_orig.getVarsInParams()
+
+    val combinations = settings.inputCombinations
+
+    println("combinations: " + combinations)
+
+    val st =
+      if (combinations.isDefined &&
+        combinations.get.head.length == vars.length)
+        (vars: Seq[ArithExpr], combinations.get.head).zipped.toMap
+      else
+        createValueMap(high_level_expr_orig)
+
+    val sizesForFilter = st.values.toSeq
+    println("sizesForFilter: " + sizesForFilter)
+
+
+    val high_level_expr = replaceInputTypes(high_level_expr_orig, st)
+
+    TypeChecker(high_level_expr)
+
+    val all_substitution_tables: Seq[SubstitutionMap] = ParameterSearch(high_level_expr)
+
+    println("List of all valid parameter sets")
+    println(all_substitution_tables)
+    println()
+    //end of new stuff!
+
+
+
+
+
+
     val lambdaStr = readFromFile(lambdaPath)
 
     //println("lambdaStr: " + lambdaStr)
