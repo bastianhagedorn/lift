d55dd158484ce84e89138364db6d991449342aa2
irgend42 <und0m3@gmail.com>
2017-07-14 19:31:32 +0200

    Lamda Analyser divides added

--- src/main/exploration/LambdaAnalyser.scala
+++ src/main/exploration/LambdaAnalyser.scala
@@ -7,8 +7,8 @@ import com.typesafe.scalalogging.Logger
 import exploration.KernelGenerator.parser
 import exploration.ParameterRewrite.{logger, parser, readLambdaFromFile, settings, _}
 import exploration.ParameterSearch.SubstitutionMap
-import ir.TypeChecker
-import ir.ast.{Expr, FunCall, Lambda}
+import ir.{ArrayType, Size, TypeChecker}
+import ir.ast.{Expr, FunCall, Gather, Lambda, ReorderWithStride, Slide, Split}
 import lift.arithmetic._
 import opencl.executor._
 import opencl.generator.NDRange
@@ -16,12 +16,11 @@ import opencl.ir.pattern._
 import org.clapper.argot._
 import rewriting.utils.Utils
 
-import scala.collection.immutable.ListMap
+import scala.collection.immutable.{ListMap, Map}
 import scala.io.Source
 import scala.util.Random
 import scala.util.parsing.json.JSONArray
 import scala.util.parsing.json.JSONObject
-
 import ExpressionFilter.Status.Success
 
 
@@ -73,8 +72,8 @@ object LambdaAnalyser {
     //new stuff!
     var hlLambdaPath = input.value.get.toPath.getParent.getParent.getParent //Lower dir
     var hlName = hlLambdaPath.getFileName.toString
-    hlLambdaPath=hlLambdaPath.getParent
-    hlLambdaPath=hlLambdaPath.getParent.resolve(hlLambdaPath.getFileName.toString.dropRight("Lower".length)).resolve(hlName.substring(0,1)).resolve(hlName.substring(1,2)).resolve(hlName)
+    hlLambdaPath = hlLambdaPath.getParent
+    hlLambdaPath = hlLambdaPath.getParent.resolve(hlLambdaPath.getFileName.toString.dropRight("Lower".length)).resolve(hlName.substring(0, 1)).resolve(hlName.substring(1, 2)).resolve(hlName)
     val settings = ParseSettings(None)
 
 
@@ -103,17 +102,7 @@ object LambdaAnalyser {
 
 
     //Weitere Parameter mit Ranges und ggf. Constraints auslesen
-    val tunables:Map[String,Map[String,Any]] = ParameterSearch2(high_level_expr)
-
-
-/*
-    val all_substitution_tables: Seq[SubstitutionMap] = ParameterSearch(high_level_expr)
-
-    println("List of all valid parameter sets")
-    println(all_substitution_tables)
-    println()
-    //end of new stuff!
-*/
+    val tunables: Map[String, Map[String, Any]] = getParameterInformation(high_level_expr)
 
 
 
@@ -123,9 +112,10 @@ object LambdaAnalyser {
     //println("lambdaStr: " + lambdaStr)
     val lowLevelFactory = Eval.getMethod(lambdaStr)
 
+    //TODO why do we pass 5 empty vars here?
     val lambda = lowLevelFactory(Seq(Var(), Var(), Var(), Var(), Var()))
 
-  /*
+    /*
     val typeChecker = TypeChecker(lambda)
     //println("typeChecker : " + typeChecker)
 
@@ -143,28 +133,28 @@ object LambdaAnalyser {
     val dims = (0 to numDimension).toList
 
     //add the GS and LS to be tuned
-    var gsls = (for (dim <- dims) yield ("gs"+dim, Map(("range", Var().range)))).toMap[String, Map[String, Object]]
-    gsls = gsls ++ (for (dim <- dims) yield ("ls"+dim, Map(("range", Var().range), ("divides", "gs"+dim)))).toMap[String, Map[String, Object]]
+    var gsls = (for (dim <- dims) yield ("gs" + dim, Map(("range", Var().range)))).toMap[String, Map[String, Object]]
+    gsls = gsls ++ (for (dim <- dims) yield ("ls" + dim, Map(("range", Var().range), ("divides", "gs" + dim)))).toMap[String, Map[String, Object]]
     //gsls = gsls ++ (for (v <- tunableVars) yield (v.toString, Map(("range", v.range)))).toMap[String, Map[String, Object]]
 
     //generate json from vars:
     var jsonList = List[JSONObject]()
     (gsls ++ tunables).foreach(
-      v=>{
+      v => {
         //never used...
         //val range = if(v._2==RangeUnknown) RangeAdd(1,1025,1) else v._2
 
-        var fields = ListMap[String,Any](
-          ("name",v._1.toString)
+        var fields = ListMap[String, Any](
+          ("name", v._1.toString)
         )
 
         v._2.foreach(
           restrictions => {
             fields = if (restrictions._2.isInstanceOf[Range]) {
-              val range = if((restrictions._2.asInstanceOf[Range])==RangeUnknown) RangeAdd(1,1025,1) else (restrictions._2.asInstanceOf[Range])
+              val range = if ((restrictions._2.asInstanceOf[Range]) == RangeUnknown) RangeAdd(1, 1025, 1) else (restrictions._2.asInstanceOf[Range])
               fields + (("interval", JSONObject(
-                ListMap[String,Any](
-                  ("type","int"),
+                ListMap[String, Any](
+                  ("type", "int"),
                   ("from", range.min),
                   ("to", range.max))
               ))
@@ -173,13 +163,13 @@ object LambdaAnalyser {
           }
         )
         val newJSONObject = JSONObject(fields)
-        jsonList = newJSONObject::jsonList
+        jsonList = newJSONObject :: jsonList
       }
     )
 
-    val output = new File(input.value.get.getParentFile,input.value.get.getName+".json")
+    val output = new File(input.value.get.getParentFile, input.value.get.getName + ".json")
     println(output)
-    val writer = new PrintWriter(new FileWriter(output,false))
+    val writer = new PrintWriter(new FileWriter(output, false))
     try writer.write(JSONArray(jsonList).toString()) finally writer.close()
 
     print("endJson: " + JSONArray(jsonList).toString())
@@ -202,25 +192,62 @@ object LambdaAnalyser {
 
   private def maxDimension(expr: Lambda): Int = {
     var usedDimensions: Set[Int] = Set()
-    Expr.visit(expr.body,
-      {
-        case FunCall(MapGlb(dim, _), _) =>
-          usedDimensions += dim
+    Expr.visit(expr.body, {
+      case FunCall(MapGlb(dim, _), _) =>
+        usedDimensions += dim
 
-        case FunCall(MapLcl(dim, _), _) =>
-          usedDimensions += dim
+      case FunCall(MapLcl(dim, _), _) =>
+        usedDimensions += dim
 
-        case FunCall(MapWrg(dim, _), _) =>
-          usedDimensions += dim
+      case FunCall(MapWrg(dim, _), _) =>
+        usedDimensions += dim
 
-        case FunCall(MapAtomLcl(dim, _, _), _) =>
-          usedDimensions += dim
+      case FunCall(MapAtomLcl(dim, _, _), _) =>
+        usedDimensions += dim
 
-        case FunCall(MapAtomWrg(dim, _, _), _) =>
-          usedDimensions += dim
+      case FunCall(MapAtomWrg(dim, _, _), _) =>
+        usedDimensions += dim
 
-        case _ =>
-      }, (_) => Unit)
+      case _ =>
+    }, (_) => Unit)
     usedDimensions.max
+  }
+
+  private def getParameterInformation(lambda: Lambda): Map[String, Map[String, Object]] = {
+    val tunableNodes = Utils.findTunableNodes(lambda)
+
+    // from that, isolate only the splits/slides
+    val splits = tunableNodes.collect({
+      case FunCall(Split(cs), x) => (cs, x.t.asInstanceOf[ArrayType with Size].size)
+      // step has to divide len - (size - step)
+      case FunCall(Slide(size, step), x) => (step, x.t.asInstanceOf[ArrayType with Size].size - (size-step))
+      case FunCall(Gather(ReorderWithStride(s)), x) if s.isInstanceOf[Var] => (s, x.t.asInstanceOf[ArrayType with Size].size)
+    })
+
+    // TODO not quite sure but I think it's better to use a mutable map here and make it immutable later on.
+    // Otherwise we have to create a new map eacht time we want to "insert" a value.
+    // maybe the way to go is simply not using foreach?
+    val result = scala.collection.mutable.Map.empty[String,Map[String,Object]]
+    splits.foreach(split =>
+      split match {
+        // If the dividend is a constant, create range and divides constraint based on that value.
+        case (v:Var, Cst(dividend)) =>
+          result += v.toString -> Map[String, Object](
+            "range"->RangeAdd(1,dividend,1),
+            "divides" -> dividend.toString
+          )
+        // If the dividend is a variable, create a divides constraint which implicitly limits the range to the range of the dividend.
+        case (v: Var, dividend: Var) =>
+          result += v.toString -> Map[String, Object](
+            "range"->RangeAdd(1,dividend,1),
+            "divides" -> dividend.toString
+          )
+        case (x,y) =>
+          println(s"Not a tunable $x, $y")
+      }
+    )
+
+
+    result.toMap
   }
 }
